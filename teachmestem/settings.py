44c009eb77cbfc8c92a0d7c897007b6ac612af67
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-04-19 12:52:24 +0100

    chore(teachmestem): set up media files

--- teachmestem/settings.py
+++ teachmestem/settings.py
@@ -134,6 +134,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles/"
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static/"]
 
+# Media files
+MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_URL = "media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
