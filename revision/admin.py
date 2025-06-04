791ca4fef5c746c25a2703f09a31398b906410f9
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-06-04 16:00:06 +0100

    feat(revision): add TutoringContactUser model to admin site

--- revision/admin.py
+++ revision/admin.py
@@ -9,8 +9,13 @@ from .models import (
     Subject,
     Topic,
     TopicCompletion,
+    TutoringContactUser,
     UserExamBoard,
 )
+
+
+class TutoringContactUserAdmin(admin.ModelAdmin):
+    list_display = ["tutor_name", "first_name", "last_name", "email"]
 
 
 class QualificationAdmin(admin.ModelAdmin):
@@ -83,6 +88,7 @@ class UserExamBoardAdmin(admin.ModelAdmin):
     ]
 
 
+admin.site.register(TutoringContactUser, TutoringContactUserAdmin)
 admin.site.register(Qualification, QualificationAdmin)
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(ExamBoard, ExamboardAdmin)
