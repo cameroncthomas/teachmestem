44c0b5f7e96f5ffd3d0bbc08fa2c88b0549255ce
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-06-04 15:16:05 +0100

    feat(revision): add TutoringContactForm form

--- revision/forms.py
+++ revision/forms.py
@@ -1,6 +1,9 @@
 from allauth.account.forms import SignupForm
+from django import forms
 from django_recaptcha.fields import ReCaptchaField
 from django_recaptcha.widgets import ReCaptchaV3
+
+from .models import TutoringContactUser
 
 
 class RevisionSignupForm(SignupForm):
@@ -17,3 +20,17 @@ class RevisionSignupForm(SignupForm):
     def save(self, request):
         user = super().save(request)
         return user
+
+
+class TutoringContactForm(forms.ModelForm):
+    captcha = ReCaptchaField(widget=ReCaptchaV3)
+
+    class Meta:
+        model = TutoringContactUser
+        fields = ["tutor_name", "first_name", "last_name", "email", "message"]
+        labels = {
+            "tutor_name": "Tutor name",
+            "email": "Email address",
+            "message": "Message",
+        }
+        widgets = {"text": forms.Textarea(attrs={"style": "height:150px"})}
