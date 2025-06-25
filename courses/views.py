5af9bf22ddb0747e8f85e8f40c59da3e139c4dc6
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-06-25 15:52:13 +0100

    feat(courses): create view function for courses page

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from revision.models import Qualification
+
+from .models import Course
+
+
+def courses(request):
+    """Show courses page."""
+    qualifications = Qualification.objects.order_by("qualification_number")
+    courses = Course.objects.select_related(
+        "instructor",
+        "examboard",
+        "examboard__subject",
+        "examboard__subject__qualification",
+    ).order_by("date", "-examboard__subject__name")
+    context = {
+        "qualifications": qualifications,
+        "courses": courses,
+    }
+    return render(request, "courses/courses.html", context)
