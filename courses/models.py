84a5df368856df0dd5c13a82d79c75f78eedd5df
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-06-25 15:43:35 +0100

    feat(courses): create Course model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from revision.models import ExamBoard
+
+
+class Course(models.Model):
+    """A course ran by an instructor for a given ExamBoard instance."""
+
+    examboard = models.ForeignKey(ExamBoard, on_delete=models.CASCADE)
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.examboard} course ran by {self.instructor} on {self.date}"
