3e80c588dca1218ea092a744749931f161886902
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-07-01 13:46:53 +0100

    feat(tutoring): register Tutor and TutorAdmin with admin site

--- tutoring/admin.py
+++ tutoring/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
-from .models import TutoringContactUser
+from .models import Tutor, TutoringContactUser
+
+
+class TutorAdmin(admin.ModelAdmin):
+    list_display = ["user", "name", "is_available"]
 
 
 class TutoringContactUserAdmin(admin.ModelAdmin):
-    list_display = ["tutor", "first_name", "last_name", "email"]
+    list_display = [
+        "tutor",
+        "first_name",
+        "last_name",
+        "email",
+    ]
 
 
+admin.site.register(Tutor, TutorAdmin)
 admin.site.register(TutoringContactUser, TutoringContactUserAdmin)
