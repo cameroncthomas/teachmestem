e46079d51f544004331025a5b84e4a93bbf6ed9d
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-07-01 13:23:11 +0100

    feat(tutoring): add Tutor model

--- tutoring/models.py
+++ tutoring/models.py
@@ -1,4 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Tutor(models.Model):
+    """A user who is a tutor."""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    text = models.TextField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.name}"
 
 
 class TutoringContactUser(models.Model):
