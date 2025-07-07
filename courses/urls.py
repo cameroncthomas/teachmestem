90dbe0fb817d21dce99c99df63e1656b5de6e171
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-07-07 13:04:01 +0100

    feat(courses): add url patterns for course registration

--- courses/urls.py
+++ courses/urls.py
@@ -13,4 +13,16 @@ urlpatterns = [
         views.courses,
         name="courses",
     ),
+    # Page that shows course registration info and form.
+    path(
+        "<int:course_id>/register/",
+        views.courses_register,
+        name="courses_register",
+    ),
+    # Page that shows course registration form success message.
+    path(
+        "<int:course_id>/register/success/",
+        views.courses_register_success,
+        name="courses_register_success",
+    ),
 ]
