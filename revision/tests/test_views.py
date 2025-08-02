56bbcab0197c6d6d0c28873a2954052ed03c1ef5
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-08-02 21:14:21 +0100

    test(revision): add tests for contact view function

--- revision/tests/test_views.py
+++ revision/tests/test_views.py
@@ -128,3 +128,45 @@ class termsViewTest(TestCase):
         self.assertQuerySetEqual(
             response.context["qualifications"], expected_qualification_order
         )
+
+
+class contactViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        number_of_qualifications = 5
+        for qualification_id in range(1, number_of_qualifications + 1):
+            Qualification.objects.create(
+                name=f"Qualification {qualification_id}",
+                qualification_number=number_of_qualifications - qualification_id + 1,
+            )
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get("/contact/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse("revision:contact"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("revision:contact"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "revision/contact.html")
+
+    def test_lists_all_qualifications(self):
+        response = self.client.get(reverse("revision:contact"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("qualifications" in response.context)
+        self.assertEqual(len(response.context["qualifications"]), 5)
+
+    def test_qualifications_ordered_by_qualification_number(self):
+        response = self.client.get(reverse("revision:contact"))
+        expected_qualification_order = sorted(
+            Qualification.objects.all(), key=lambda q: q.qualification_number
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("qualifications" in response.context)
+        self.assertQuerySetEqual(
+            response.context["qualifications"], expected_qualification_order
+        )
