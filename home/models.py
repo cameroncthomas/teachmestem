558dd86d445473c542a5dc7632bce3b0163be26b
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-10-16 17:46:45 +0100

    feat: move ContactUser model from revision to home

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactUser(models.Model):
+    """A user who contacts via the Contact Form."""
+
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    text = models.TextField(max_length=10000)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.first_name} {self.last_name}"
--- revision/models.py
+++ revision/models.py
@@ -3,19 +3,6 @@ from django.db import models
 from django.utils.text import slugify
 
 
-class ContactUser(models.Model):
-    """A user who contacts via the Contact Form."""
-
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=254)
-    text = models.TextField(max_length=10000)
-
-    def __str__(self):
-        """Return a string representation of the model."""
-        return f"{self.first_name} {self.last_name}"
-
-
 class Qualification(models.Model):
     """A qualification one may be studying."""
 
