8bbef99d067c8b256c1a502554072b9c9c9db954
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-10-16 17:44:07 +0100

    feat: move views from revision to home app

--- home/views.py
+++ home/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import ContactForm
+
+
+def index(request):
+    """Home page for revision app."""
+    return render(request, "revision/index.html")
+
+
+def privacy(request):
+    """Show privacy page."""
+    return render(request, "revision/privacy.html")
+
+
+def terms(request):
+    """Show terms page."""
+    return render(request, "revision/terms.html")
+
+
+def contact(request):
+    """Show contact page."""
+
+    if request.method != "POST":
+        form = ContactForm()
+    else:
+        form = ContactForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            send_mail(
+                subject=f"{form.cleaned_data['first_name']} {form.cleaned_data['last_name']} has sent a contact request",
+                message=f"""
+                    Name: {form.cleaned_data["first_name"]} {form.cleaned_data["last_name"]}
+                    Email: {form.cleaned_data["email"]}
+                    Message:
+                    
+                    {form.cleaned_data["text"]}
+                    
+                    """,
+                from_email=None,
+                recipient_list=[],
+            )
+            return redirect("revision:contact_sent")
+
+    context = {
+        "form": form,
+    }
+    return render(request, "revision/contact.html", context)
+
+
+def contact_sent(request):
+    """Show contact form success page."""
+    return render(request, "revision/contact_sent.html")
--- revision/views.py
+++ revision/views.py
@@ -1,11 +1,9 @@
 from allauth.account.decorators import verified_email_required
 from django.contrib.auth.decorators import login_required
-from django.core.mail import send_mail
 from django.http import FileResponse, Http404
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.utils.text import slugify
 
-from .forms import ContactForm
 from .models import (
     ExamBoard,
     PastPaper,
@@ -16,56 +14,6 @@ from .models import (
     TopicCompletion,
     UserExamBoard,
 )
-
-
-def index(request):
-    """Home page for revision app."""
-    return render(request, "revision/index.html")
-
-
-def privacy(request):
-    """Show privacy page."""
-    return render(request, "revision/privacy.html")
-
-
-def terms(request):
-    """Show terms page."""
-    return render(request, "revision/terms.html")
-
-
-def contact(request):
-    """Show contact page."""
-
-    if request.method != "POST":
-        form = ContactForm()
-    else:
-        form = ContactForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            send_mail(
-                subject=f"{form.cleaned_data['first_name']} {form.cleaned_data['last_name']} has sent a contact request",
-                message=f"""
-                    Name: {form.cleaned_data["first_name"]} {form.cleaned_data["last_name"]}
-                    Email: {form.cleaned_data["email"]}
-                    Message:
-                    
-                    {form.cleaned_data["text"]}
-                    
-                    """,
-                from_email=None,
-                recipient_list=[],
-            )
-            return redirect("revision:contact_sent")
-
-    context = {
-        "form": form,
-    }
-    return render(request, "revision/contact.html", context)
-
-
-def contact_sent(request):
-    """Show contact form success page."""
-    return render(request, "revision/contact_sent.html")
 
 
 @login_required
