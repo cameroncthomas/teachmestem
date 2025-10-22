153a91086db14718808c9fdd9c5f15749cb93f08
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-10-22 14:44:51 +0100

    test(revision): add tests for my_subjects view

--- revision/tests/test_views.py
+++ revision/tests/test_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from revision.models import ExamBoard, Qualification, Subject
+from revision.models import ExamBoard, Qualification, Subject, UserExamBoard
 
 User = get_user_model()
 
@@ -291,3 +291,85 @@ class SubjectViewTest(TestCase):
         self.assertEqual(str(response.context["user"]), "jdoe137")
         self.assertTrue("examboards" in response.context)
         self.assertEqual(len(response.context["examboards"]), 7)
+
+
+class MySubjectsViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        number_of_qualifications = 5
+        for qualification_id in range(1, number_of_qualifications + 1):
+            Qualification.objects.create(
+                name=f"Qualification {qualification_id}",
+                qualification_number=number_of_qualifications - qualification_id + 1,
+            )
+        qualification = Qualification.objects.get(id=1)
+        Subject.objects.create(
+            qualification=qualification, name="Maths", subject_number=1
+        )
+        subject = Subject.objects.get(id=1)
+        number_of_examboards = 7
+        for examboard_id in range(1, number_of_examboards + 1):
+            ExamBoard.objects.create(
+                subject=subject,
+                name=f"Examboard {examboard_id}",
+            )
+        user = User.objects.create_user(
+            username="jdoe137",
+            email="test@email.com",
+            password="password137",
+            first_name="John",
+            last_name="Doe",
+        )
+        number_of_my_subjects = 5
+        for my_subject_id in range(1, number_of_my_subjects + 1):
+            UserExamBoard.objects.create(
+                user=user, examboard=ExamBoard.objects.get(id=my_subject_id)
+            )
+
+    def test_redirects_if_not_logged_in(self):
+        response = self.client.get(reverse("revision:my_subjects"))
+        self.assertRedirects(response, "/accounts/login/?next=/revise/my-subjects/")
+
+    def test_view_url_exists_at_desired_location(self):
+        self.client.login(username="jdoe137", password="password137")
+        response = self.client.get("/revise/my-subjects/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        self.client.login(username="jdoe137", password="password137")
+        response = self.client.get(reverse("revision:my_subjects"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        self.client.login(username="jdoe137", password="password137")
+        response = self.client.get(reverse("revision:my_subjects"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "revision/my_subjects.html")
+
+    def test_lists_all_qualifications(self):
+        self.client.login(username="jdoe137", password="password137")
+        response = self.client.get(reverse("revision:my_subjects"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("qualifications" in response.context)
+        self.assertEqual(len(response.context["qualifications"]), 5)
+
+    def test_qualifications_ordered_by_qualification_number(self):
+        self.client.login(username="jdoe137", password="password137")
+        response = self.client.get(reverse("revision:my_subjects"))
+        expected_qualification_order = sorted(
+            Qualification.objects.all(), key=lambda q: q.qualification_number
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("qualifications" in response.context)
+        self.assertQuerySetEqual(
+            response.context["qualifications"], expected_qualification_order
+        )
+
+    def test_lists_all_my_subjects(self):
+        self.client.login(username="jdoe137", password="password137")
+        user = User.objects.get(id=1)
+        my_subjects = UserExamBoard.objects.filter(user=user)
+        response = self.client.get(reverse("revision:my_subjects"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("my_subjects" in response.context)
+        self.assertEqual(len(response.context["my_subjects"]), 5)
