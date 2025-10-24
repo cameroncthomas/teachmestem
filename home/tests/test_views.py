c11914d029fb0a46741706ed7335be83fb1a98cb
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-10-24 14:04:15 +0100

    test(home): add tests for my_account view

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
 from revision.models import Qualification
+
+User = get_user_model()
 
 
 class IndexViewTest(TestCase):
@@ -199,6 +202,63 @@ class ContactSentViewTest(TestCase):
 
     def test_qualifications_ordered_by_qualification_number(self):
         response = self.client.get(reverse("home:contact_sent"))
+        expected_qualification_order = sorted(
+            Qualification.objects.all(), key=lambda q: q.qualification_number
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("qualifications" in response.context)
+        self.assertQuerySetEqual(
+            response.context["qualifications"], expected_qualification_order
+        )
+
+
+class MyAccountViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        number_of_qualifications = 5
+        for qualification_id in range(1, number_of_qualifications + 1):
+            Qualification.objects.create(
+                name=f"Qualification {qualification_id}",
+                qualification_number=number_of_qualifications - qualification_id + 1,
+            )
+        User.objects.create_user(
+            username="jdoe137",
+            email="test@email.com",
+            password="password137",
+            first_name="John",
+            last_name="Doe",
+        )
+
+    def test_redirects_if_not_logged_in(self):
+        response = self.client.get(reverse("home:my_account"))
+        self.assertRedirects(response, "/accounts/login/?next=/my-account/")
+
+    def test_view_url_exists_at_desired_location(self):
+        self.client.login(username="jdoe137", password="password137")
+        response = self.client.get("/my-account/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        self.client.login(username="jdoe137", password="password137")
+        response = self.client.get(reverse("home:my_account"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        self.client.login(username="jdoe137", password="password137")
+        response = self.client.get(reverse("home:my_account"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/my_account.html")
+
+    def test_lists_all_qualifications(self):
+        self.client.login(username="jdoe137", password="password137")
+        response = self.client.get(reverse("home:my_account"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("qualifications" in response.context)
+        self.assertEqual(len(response.context["qualifications"]), 5)
+
+    def test_qualifications_ordered_by_qualification_number(self):
+        self.client.login(username="jdoe137", password="password137")
+        response = self.client.get(reverse("home:my_account"))
         expected_qualification_order = sorted(
             Qualification.objects.all(), key=lambda q: q.qualification_number
         )
